3bc3c2e855d4ec5d709c151528e818d2e8c93520
Dolapo Oguntoyinbo <Dolapo.Oguntoyinbo@Interswitchng.com>
2023-09-10 20:51:53 +0100

    Updated file

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -1,5 +1,99 @@
 package api.test;
 
-public class UserTests {
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-}
+import com.github.javafaker.Faker;
+
+import api.endpoints.UserEndPoints;
+import api.payload.User;
+import io.restassured.response.Response;
+
+
+public class UserTests {
+	
+	Faker faker;
+	User userPayload;
+	
+	@BeforeClass
+	public void setupData() 
+	{
+		faker=new Faker();
+		userPayload=new User();
+		
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		userPayload.setPassword(faker.internet().password(5,10));
+		userPayload.setPhone(faker.phoneNumber().cellPhone());
+
+	}
+	@Test(priority=1)
+	
+	public void testPostUser() 
+	{
+		Response response=UserEndPoints.createUser(userPayload);
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
+	@Test(priority=2)
+	public void testGetUserByName() 
+	{
+		Response response=UserEndPoints.readUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(),200);
+	}
+	@Test
+	public void testUpdateUserByName() 
+	{
+	
+		//update data using payload
+		
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		
+		Response response=UserEndPoints.updateUser(this.userPayload.getUsername(),userPayload);
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		//Checking data  after update
+		
+		Response responseAfterupdate=UserEndPoints.readUser(this.userPayload.getUsername());
+		Assert.assertEquals(responseAfterupdate.getStatusCode(), 200);
+	}
+	@Test
+	public void testDeleteUserByName() 
+	{
+		Response response=UserEndPoints.deleteUser(this.userPayload.getUsername());
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
+	
+	@Test
+	public void testHeaders() {
+	    Response response = UserEndPoints.readUser(this.userPayload.getUsername());
+	    response.then().log().all();
+	    
+	    // Content-Type is application/json
+	    response.then().header("Content-Type", "application/json");
+	}
+
+	@Test
+	public void testErrorHandling() {
+	    String invalidUsername = "invalidUsername";
+	    
+	    Response response = UserEndPoints.readUser(invalidUsername);
+	    
+	    response.then().log().all();
+	    
+	    // Assertion for error status code 
+	    Assert.assertEquals(response.getStatusCode(), 404);
+	    
+	}
+
+	}
--- src/test/java/api/utilities/XLUtility.java
+++ src/test/java/api/utilities/XLUtility.java
@@ -1,5 +1,32 @@
-package api.utilities;
-
-public class XLUtility {
-
-}
+//package api.utilities;
+//
+//import java.io.FileInputStream;
+//
+//import org.apache.poi.xssf.usermodel.XSSFSheet;
+//
+//public class XLUtility {
+//	public FileInputStream fi;
+//	public FileInputStream fo;
+//	public XSSWorkbook workbook;
+//	public XSSFSheet sheet;
+//	public XSSFCell cell;
+//	public XSSFRow row;
+//	public CellStyle;
+//	String path;
+//	
+//public XLUtility(String path) {
+//	this.path=path;
+//}
+//
+//public int getRowCount(String sheerName) throws IOException{
+//	fi=new FileInputStream(path);
+//	workbook=new XSSFWorkbook(fi);
+//	sheet=workbook.getSheet(sheetName);
+//	int rowcount=sheet.getLastRowNum();
+//	workbook.close();
+//	fi.close();
+//	return rowcount;
+//}
+//
+//public int getCellCount(String sheetName., int rownum)
+//}
